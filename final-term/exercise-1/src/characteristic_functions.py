047b8c83a7cc943d1661dcd7d8335e1c0e3cfa5a
LordCatello <carmine_carratu@libero.it>
2021-06-09 13:34:57 +0200

    Implementend characteristic functions

---
+++ final-term/exercise-1/src/characteristic_functions.py
@@ -0,0 +1,55 @@
+import copy
+import typing
+import networkx as nx
+
+
+def degree(graph: nx.Graph, coalition: typing.Set) -> float:
+    added = copy.deepcopy(coalition)
+
+    for node in coalition:
+        neighbours = graph.neighbors(node)
+        for neighbour in neighbours:
+            added.add(neighbour)
+
+    return len(added)
+
+
+def threshold(graph: nx.Graph, coalition: typing.Set, threshold_value: int) -> float:
+    added = copy.deepcopy(coalition)
+
+    for node in coalition:
+        neighbours = graph.neighbors(node)
+        for neighbour in neighbours:
+            # check threshold
+            if _coalition_neighbours(graph, coalition, neighbour) >= threshold_value:
+                added.add(neighbour)
+
+    return len(added)
+
+
+# chiedere per sicurezza a Ferraioli se la closeness è quello che intendiamo noi
+def closeness(graph: nx.Graph, coalition: typing.Set) -> float:
+    value = 0
+
+    for node in graph.nodes():
+        if node not in coalition:
+            # compute distance between all the nodes in the coalition and take the minimum distance
+            min_distance = float("inf")
+            for coalition_node in coalition:
+                distance = nx.shortest_path_length(graph, coalition_node, node)
+                if distance < min_distance:
+                    min_distance = distance
+
+            value += min_distance
+
+    return value
+
+
+def _coalition_neighbours(graph: nx.Graph, coalition: typing.Set, node: int) -> int:
+    num_neighbours = 0
+
+    for neighbour in graph.neighbors(node):
+        if neighbour in coalition:
+            num_neighbours += 1
+
+    return num_neighbours
---
+++ final-term/exercise-1/test/test_characteristic_functions.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+import networkx as nx
+import characteristic_functions
+
+
+class TestCharacteristicFunctions(TestCase):
+    def setUp(self) -> None:
+        self.graph = nx.Graph()
+        self.graph.add_edge(1, 2)
+        self.graph.add_edge(1, 4)
+        self.graph.add_edge(2, 4)
+        self.graph.add_edge(2, 5)
+        self.graph.add_edge(3, 5)
+        self.graph.add_edge(4, 5)
+
+        self.coalitions = [
+            {1},
+            {3},
+            {2, 4},
+            {2, 4, 5}
+        ]
+
+    def test_degree(self):
+        expected_values = [3, 2, 4, 5]
+        for expected, coalition in zip(expected_values, self.coalitions):
+            actual = characteristic_functions.degree(self.graph, coalition)
+            self.assertEqual(expected, actual)
+
+    def test_threshold(self):
+        threshold = 2
+        expected_values = [1, 1, 4, 4]
+        for expected, coalition in zip(expected_values, self.coalitions):
+            actual = characteristic_functions.threshold(self.graph, coalition, threshold)
+            self.assertEqual(expected, actual)
+
+    def test_closeness(self):
+        expected_values = [7, 8, 4, 2]
+        for expected, coalition in zip(expected_values, self.coalitions):
+            actual = characteristic_functions.closeness(self.graph, coalition)
+            self.assertEqual(expected, actual)
