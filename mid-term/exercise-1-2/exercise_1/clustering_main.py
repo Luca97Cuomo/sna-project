57eefd8536aaaa3d9570e77ea068e5b5e8002525
Davide Cafaro <dvdcafaro@gmail.com>
2021-06-13 15:25:53 +0200

    parallel girvan newman

--- mid-term/exercise-1-2/exercise_1/clustering_main.py
+++ mid-term/exercise-1-2/exercise_1/clustering_main.py
@@ -15,7 +15,7 @@ def main():
     graph, true_clusters = utils.load_graph_and_clusters(PATH_TO_NODES, PATH_TO_EDGES)
 
     for i, cluster in enumerate(true_clusters):
-        logger.info(f'The length of the real cluster_{i+1} is: {len(cluster)}')
+        logger.info(f'The length of the real cluster_{i + 1} is: {len(cluster)}')
 
     # clustering = clustering_utils.Clustering([
     #     (clustering_algorithms.hierarchical_optimized, {"seed": 42, "desired_clusters": 4}),
@@ -42,22 +42,9 @@ def main():
     # clustering.evaluate_all()
 
     clustering = clustering_utils.Clustering([
-        (clustering_algorithms.k_means_one_iteration, {"seed": 42, "k": 4}),
-        (clustering_algorithms.k_means, {"centrality_measure": None, "seed": 42, "k": 4,
-                                         "equality_threshold": 0.001, "max_iterations": 1,
-                                         "centers": None}),
-        (clustering_algorithms.k_means, {"centrality_measure": None, "seed": 42, "k": 4,
-                                         "equality_threshold": 0.001, "max_iterations": 10000,
-                                         "centers": None}),
-        (clustering_algorithms.k_means, {"centrality_measure": "degree_centrality", "seed": 42, "k": 4,
-                                         "equality_threshold": 0.001, "max_iterations": 10000,
-                                         "centers": None}),
-        (clustering_algorithms.k_means, {"centrality_measure": "nodes_betweenness_centrality", "seed": 42, "k": 4,
-                                         "equality_threshold": 0.001, "max_iterations": 10,
-                                         "centers": None}),
-        (clustering_algorithms.k_means, {"centrality_measure": "pagerank", "seed": 42, "k": 4,
-                                         "equality_threshold": 0.001, "max_iterations": 50,
-                                         "centers": None})],
+        (clustering_algorithms.girvan_newman, {"centrality_measure": "parallel_edges_betweenness_centrality", "seed": 42,
+                                               "k": 4, "optimized": False})
+    ],
         graph, true_clusters, draw_graph=False)
 
     clustering.evaluate_all()
